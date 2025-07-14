550bf1a368cc20aba7d26ce5737d9165faff8539
Michael Achumba <michaelachumba@gmail.com>
2025-07-14 01:06:13 +0100

    Writes a script that computes and prints a factorial

---
+++ 10-factorial.js
@@ -0,0 +1,11 @@
+// convert first argument to integer
+const num = parseInt(process.argv[2]);
+
+function factorial(n) {
+    if (isNaN(n) || n === 1) {
+        return 1;
+    }
+    return n * factorial(n - 1);
+}
+
+console.log(factorial(num));
